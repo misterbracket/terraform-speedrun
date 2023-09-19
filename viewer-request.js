a3aecd1c235ef0ef37f3490f23e1776f63397193
Max Klammer <maximilian.klammer@gmail.com>
2023-09-19 09:09:11 +0200

    refactored function

--- viewer-request.js
+++ viewer-request.js
@@ -1,20 +1,28 @@
-function handler(event) {
-  var request = event.request;
-  var routes = request.uri.split("/");
+// Basic routing helper to serve the right html file from s3
+// assume uri to be like /**/* or /**/*/ that require modification
+// modify uri by appending .html so that the right object can be found in s3
 
-  // assume uri to be like /**/* or /**/*/ that require modification
-  // modify uri by appending .html so that the right object can be found in s3
-  if (routes[routes.length - 1] === "") {
-    routes.pop();
+function handler(event) {
+  const request = event.request;
+  const pathSegments = request.uri.split("/");
+
+  //Pop off the last segment if it is empty (trailing slash)
+  if (pathSegments[pathSegments.length - 1] === "") {
+    pathSegments.pop();
   }
 
-  if (routes.length > 0) {
-    var lastRoute = routes[routes.length - 1];
-    if (lastRoute !== "" && !routes[routes.length - 1].includes(".")) {
-      routes[routes.length - 1] += ".html";
+  //Append .html to the last segment if it does not contain a period
+  if (pathSegments.length > 0) {
+    const lastRoute = pathSegments[pathSegments.length - 1];
+    if (
+      lastRoute !== "" &&
+      !pathSegments[pathSegments.length - 1].includes(".")
+    ) {
+      pathSegments[pathSegments.length - 1] += ".html";
     }
 
-    var newUri = routes.join("/");
+    //Rebuild the URI and add a leading slash
+    let newUri = pathSegments.join("/");
     if (!newUri.startsWith("/")) {
       newUri = "/" + newUri;
     }
